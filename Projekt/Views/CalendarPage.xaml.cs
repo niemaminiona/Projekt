78bcfb7dec41a9ef2c374566ee5acdaaf74c44f2
Kuba <Kuba>
2025-11-16 17:43:53 +0100

    Stworzono na podstronie kalendarza Klase miesiac oraz konstruktor domyslny z nazwą miesiaca oraz liczba dni. Stworzono funkcję dodającą do listy miesiecy typu powyzszej klasy dane dla kazdego miesiaca w roku. wywolano funkcję.

--- Projekt/Views/CalendarPage.xaml.cs
+++ Projekt/Views/CalendarPage.xaml.cs
@@ -5,5 +5,40 @@ public partial class CalendarPage : ContentPage
 	public CalendarPage()
 	{
 		InitializeComponent();
+		CreateMonthsList();
 	}
+
+	private class Month
+	{
+		public string? Name { get; set; }
+		public int Days { get; set; }
+
+        public Month(string Name, int Days)
+        {
+            this.Name = Name;
+			this.Days = Days;
+        }
+    }
+
+	List<Month> Months = new List<Month>();
+
+	private void CreateMonthsList()
+	{
+		Months.Add(new Month("January", 31));
+		Months.Add(new Month("February", 28));
+		Months.Add(new Month("March", 31));
+		Months.Add(new Month("April", 30));
+		Months.Add(new Month("May", 31));
+		Months.Add(new Month("June", 30));
+		Months.Add(new Month("July", 31));
+		Months.Add(new Month("August", 31));
+		Months.Add(new Month("September", 30));
+		Months.Add(new Month("October", 31));
+		Months.Add(new Month("November", 30));
+		Months.Add(new Month("December", 31));
+    }
+	
+
+
+
 }
