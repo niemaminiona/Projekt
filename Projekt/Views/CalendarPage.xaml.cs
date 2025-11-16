78a8f0f3ed5da1e3cbc9321282e587b968c21a87
Kuba <Kuba>
2025-11-16 18:23:38 +0100

    Zamiana nazw miesiecy z angielskich na polskie

--- Projekt/Views/CalendarPage.xaml.cs
+++ Projekt/Views/CalendarPage.xaml.cs
@@ -24,18 +24,18 @@ public partial class CalendarPage : ContentPage
 
 	private void CreateMonthsList()
 	{
-		Months.Add(new Month("January", 31));
-		Months.Add(new Month("February", 28));
-		Months.Add(new Month("March", 31));
-		Months.Add(new Month("April", 30));
-		Months.Add(new Month("May", 31));
-		Months.Add(new Month("June", 30));
-		Months.Add(new Month("July", 31));
-		Months.Add(new Month("August", 31));
-		Months.Add(new Month("September", 30));
-		Months.Add(new Month("October", 31));
-		Months.Add(new Month("November", 30));
-		Months.Add(new Month("December", 31));
+		Months.Add(new Month("Styczeñ", 31));
+		Months.Add(new Month("Luty", 28));
+		Months.Add(new Month("Marzec", 31));
+		Months.Add(new Month("Kwiecieñ", 30));
+		Months.Add(new Month("Maj", 31));
+		Months.Add(new Month("Czerwiec", 30));
+		Months.Add(new Month("Lipiec", 31));
+		Months.Add(new Month("Sierpieñ", 31));
+		Months.Add(new Month("Wrzeœieñ", 30));
+		Months.Add(new Month("PaŸdziernik", 31));
+		Months.Add(new Month("Listopad", 30));
+		Months.Add(new Month("Grudzieñ", 31));
     }
 	
 
