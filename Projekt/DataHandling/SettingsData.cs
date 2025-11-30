4647bef2f7ff1e2cc33879fc289f8d242ed539a8
niemaminionaPC <f11w@o2.pl>
2025-11-30 14:28:29 +0100

    Dodano klase SettingsData ktora bedzie przechowywac dane dla ustawien

---
+++ Projekt/DataHandling/SettingsData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt.DataHandling
+{
+    internal class SettingsData
+    {
+        public static bool NotificationEnabled = true;
+        public static short Language = 0;
+        public static short Theme = 0;
+    }
+}
