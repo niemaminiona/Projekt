4610f3d65c9adbc78d333d26b77d22b757e7b23b
jakub.wojcik <jakub.wojcik@S50-08>
2025-12-09 08:08:51 +0100

    Dodano nowy folder do API i klase API

---
+++ Projekt/Services/SupplementApiService.cs
@@ -0,0 +1,33 @@
+﻿using Projekt.DataHandling;
+using System.Net.Http.Json;
+namespace Projekt.Services
+{
+    public static class SupplementApiService
+    {
+        private static readonly HttpClient client = new HttpClient()
+        {
+            Timeout = TimeSpan.FromSeconds(10)
+        };
+
+
+        private const string API_URL =
+    "https://world.openfoodfacts.net/cgi/search.pl?json=1&search_terms=";
+
+
+
+        public static async Task<List<Suplement>> SearchAsync(string query)
+        {
+            try
+            {
+                string url = API_URL + Uri.EscapeDataString(query);
+                var result = await client.GetFromJsonAsync<List<Suplement>>(url);
+
+                return result ?? new List<Suplement>();
+            }
+            catch
+            {
+                return new List<Suplement>();
+            }
+        }
+    }
+}
